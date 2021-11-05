0b70e377db7622bef293c1836c9e7b08bfc217a6
ST John <st--@users.noreply.github.com>
2021-11-05 16:34:34 +0200

    update optimize_loss utility function

--- api_testing/utils_optimize_loss.jl
+++ api_testing/utils_optimize_loss.jl
@@ -1,39 +1,40 @@
 using Optim, Zygote, ParameterHandling
 
-function optimize_loss(loss, θ_init; iterations=1_000)
-    # L-BFGS parameters chosen because they seems to work well empirically.
-    # You could also try with the defaults.
-    optimizer = LBFGS(;
-            alphaguess=Optim.LineSearches.InitialStatic(; scaled=true),
-            linesearch=Optim.LineSearches.BackTracking(),
-    )
-	options = Optim.Options(; iterations, show_trace=true)
+# L-BFGS parameters chosen because they seem to work well empirically.
+# You could also try with the defaults.
+default_optimizer = LBFGS(;
+    alphaguess=Optim.LineSearches.InitialStatic(; scaled=true),
+    linesearch=Optim.LineSearches.BackTracking(),
+)
+
+function optimize_loss(loss, θ_init; optimizer=default_optimizer, maxiter=1_000)
+    options = Optim.Options(; iterations=maxiter, show_trace=true)
 
     θ_flat_init, unflatten = ParameterHandling.value_flatten(θ_init)
-	loss_packed = loss ∘ unflatten 
+    loss_packed = loss ∘ unflatten 
 
-	# https://julianlsolvers.github.io/Optim.jl/stable/#user/tipsandtricks/#avoid-repeating-computations
-	function fg!(F, G, x)
-	    if F != nothing && G != nothing
-			val, grad = Zygote.withgradient(loss_packed, x)
-			G .= only(grad)
-			return val
-		elseif G != nothing
-			grad = Zygote.gradient(loss_packed, x)
-			G .= only(grad)
+    # https://julianlsolvers.github.io/Optim.jl/stable/#user/tipsandtricks/#avoid-repeating-computations
+    function fg!(F, G, x)
+        if F != nothing && G != nothing
+            val, grad = Zygote.withgradient(loss_packed, x)
+            G .= only(grad)
+            return val
+        elseif G != nothing
+            grad = Zygote.gradient(loss_packed, x)
+            G .= only(grad)
             return nothing
-		elseif F != nothing
-	    	return loss_packed(x)
-	  	end
-	end
+        elseif F != nothing
+            return loss_packed(x)
+          end
+    end
 
-	result = optimize(
-		Optim.only_fg!(fg!),
-		θ_flat_init,
-		optimizer,
-		options;
-		inplace=false,
-	)
+    result = optimize(
+        Optim.only_fg!(fg!),
+        θ_flat_init,
+        optimizer,
+        options;
+        inplace=false,
+    )
 
     return unflatten(result.minimizer), result
 end
