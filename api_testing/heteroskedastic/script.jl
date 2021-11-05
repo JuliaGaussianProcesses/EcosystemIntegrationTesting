fe96d9778128c9daac7376c3d99264ae408f9d1c
ST John <st--@users.noreply.github.com>
2021-11-05 12:41:27 +0200

    move optimize_loss into utility file

--- api_testing/heteroskedastic/script.jl
+++ api_testing/heteroskedastic/script.jl
@@ -176,42 +176,7 @@ val, grad = Zygote.withgradient(loss ∘ ParameterHandling.value ∘ unflatten, 
 only(grad)
 
 # ╔═╡ 256f04e6-00d9-4dee-92f6-5d37d8f1ebfa
-function optimize_loss(loss, θ_init; iterations=1_000)
-    # L-BFGS parameters chosen because they seems to work well empirically.
-    # You could also try with the defaults.
-    optimizer = LBFGS(;
-            alphaguess=Optim.LineSearches.InitialStatic(; scaled=true),
-            linesearch=Optim.LineSearches.BackTracking(),
-    )
-	options = Optim.Options(; iterations, show_trace=true)
-
-    θ_flat_init, unflatten = ParameterHandling.value_flatten(θ_init);
-	loss_packed = loss ∘ unflatten 
-
-	# https://julianlsolvers.github.io/Optim.jl/stable/#user/tipsandtricks/#avoid-repeating-computations
-	function fg!(F, G, x)
-	    if F != nothing && G != nothing
-			val, grad = Zygote.withgradient(loss_packed, x)
-			G .= only(grad)
-			return val
-		elseif G != nothing
-			grad = Zygote.gradient(loss_packed, x)
-			G .= only(grad)
-		elseif F != nothing
-	    	return loss_packed(x)
-	  	end
-	end
-
-	result = optimize(
-		Optim.only_fg!(fg!),
-		θ_flat_init,
-		optimizer,
-		options;
-		inplace=false,
-	)
-
-    return unflatten(result.minimizer), result
-end
+include("../utils_optimize_loss.jl")
 
 # ╔═╡ afaf21f5-f687-470c-a00a-3c5cfb691d30
 begin
---
+++ api_testing/utils_optimize_loss.jl
@@ -0,0 +1,38 @@
+using Optim, Zygote, ParameterHandling
+
+function optimize_loss(loss, θ_init; iterations=1_000)
+    # L-BFGS parameters chosen because they seems to work well empirically.
+    # You could also try with the defaults.
+    optimizer = LBFGS(;
+            alphaguess=Optim.LineSearches.InitialStatic(; scaled=true),
+            linesearch=Optim.LineSearches.BackTracking(),
+    )
+	options = Optim.Options(; iterations, show_trace=true)
+
+    θ_flat_init, unflatten = ParameterHandling.value_flatten(θ_init);
+	loss_packed = loss ∘ unflatten 
+
+	# https://julianlsolvers.github.io/Optim.jl/stable/#user/tipsandtricks/#avoid-repeating-computations
+	function fg!(F, G, x)
+	    if F != nothing && G != nothing
+			val, grad = Zygote.withgradient(loss_packed, x)
+			G .= only(grad)
+			return val
+		elseif G != nothing
+			grad = Zygote.gradient(loss_packed, x)
+			G .= only(grad)
+		elseif F != nothing
+	    	return loss_packed(x)
+	  	end
+	end
+
+	result = optimize(
+		Optim.only_fg!(fg!),
+		θ_flat_init,
+		optimizer,
+		options;
+		inplace=false,
+	)
+
+    return unflatten(result.minimizer), result
+end
